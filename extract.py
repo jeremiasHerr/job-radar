7cece88d7dec9b32e2d3c7eba811a4accfa39474
Jeremias Herrera <jherrera.nqn@gmail.com>
2026-09-11 19:28:35 -0300

    fix: use word boundaries to avoid substring false positives

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import os
 import psycopg
+import re
 from psycopg.rows import dict_row
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
@@ -15,7 +16,8 @@ def find_occurrence(text):
     occurrences = set()
     text = text.lower()
     for tech in TECH_LIST:
-        if tech in text:
+        pattern = rf"\b{tech}\b"
+        if re.search(pattern, text):
             occurrences.add(tech)
     return occurrences
 
@@ -26,7 +28,7 @@ def main():
     with conn.cursor(row_factory=dict_row) as cur:
         cur.execute("""SELECT id, description, desirable, responsibilities FROM jobs """)
         row = cur.fetchone()
-        soup = BeautifulSoup(row["description"], "html.parser")
+        soup = BeautifulSoup(row["desirable"], "html.parser")
         clean_text = soup.get_text(separator=" ", strip=True)
         print(clean_text)
         print(find_occurrence(clean_text))
