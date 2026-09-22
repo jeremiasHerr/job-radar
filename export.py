64dd583ff7fce796643489357866d1b239538532
jeremiasHerr <jherrera.nqn@gmail.com>
2026-09-22 15:04:36 -0300

    fix: fix data.json

--- export.py
+++ export.py
@@ -24,7 +24,7 @@ def main():
             "jobs": jobs
         }
     conn.close()
-    with open("data.json","w", encoding="utf-8") as f:
+    with open("./public/data.json","w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, default=serialize, indent=2)     
 
 def serialize(obj):
